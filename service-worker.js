52f4fa607b940d4900c24dc4796980770c74b949
Oliver-Marcus Eder <github@derkonzern.com>
2025-06-08 17:14:39 +0200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,9 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+    .then(function(registration) {
+      console.log('Service Worker registriert:', registration.scope);
+    })
+    .catch(function(err) {
+      console.log('Service Worker Registrierung fehlgeschlagen:', err);
+    });
+}
