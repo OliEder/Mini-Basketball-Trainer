6501d89d0de7ab18f5a586f501d89a0bc50342b5
Oliver-Marcus Eder <github@derkonzern.com>
2025-06-08 17:17:38 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,10 @@
+self.addEventListener('fetch', function(event) {
+  console.log('Service Worker fetch event');
+});
+
+self.addEventListener('install', function(event) {
+  console.log('Service Worker installed');
+});
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('./service-worker.js')
     .then(function(registration) {
